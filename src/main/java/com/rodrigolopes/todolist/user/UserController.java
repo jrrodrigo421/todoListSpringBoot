5cbd94b249080c2957eb1067ad60286a0f6c5151
jrrodrigo421 <rjr89000@gmail.com>
2023-10-13 20:06:57 -0300

    adicionando verificação de usuario existente e tratamento da resposta da requisição

--- src/main/java/com/rodrigolopes/todolist/user/UserController.java
+++ src/main/java/com/rodrigolopes/todolist/user/UserController.java
@@ -1,5 +1,8 @@
 package com.rodrigolopes.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,11 +12,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
 
+  @Autowired
+  private IUserRepository userRepository;
+
   @PostMapping("/")
-  public void create(@RequestBody UserModel userModel) {
+  public ResponseEntity create(@RequestBody UserModel userModel) {
+
+    var user = this.userRepository.findByUsername(userModel.getUsername());
+
+    if (user != null) {
+      System.out.println("Usuário já existe");
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe!!!");
+
+    }
+
     System.out.println("IMPRIMINDO usermodel" + userModel.getName());
     System.out.println("IMPRIMINDO usermodel" + userModel.getUsername());
     System.out.println("IMPRIMINDO usermodel" + userModel.getPassword());
     System.out.println("IMPRIMINDO usermodel" + userModel.getClass());
+
+    var userCreated = this.userRepository.save(userModel);
+    return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
   }
 }
