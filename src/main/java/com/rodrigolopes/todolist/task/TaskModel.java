17ed8af118211916538515367bde10f74e5bb616
jrrodrigo421 <rjr89000@gmail.com>
2023-10-14 15:08:20 -0300

    tratamento de Exception

--- src/main/java/com/rodrigolopes/todolist/task/TaskModel.java
+++ src/main/java/com/rodrigolopes/todolist/task/TaskModel.java
@@ -21,12 +21,23 @@ public class TaskModel {
 
   @Column(length = 50)
   private String title;
+
   private LocalDateTime startAt;
+
   private LocalDateTime endAt;
+
   private String priority;
 
   @CreationTimestamp
   private LocalDateTime createAt;
+
   private UUID idUser;
 
+  public void setTitle(String title) throws Exception {
+    if (title.length() > 50) {
+      throw new Exception("O campo title deve conter no máximo 50 caracteres");
+
+    }
+    this.title = title;
+  }
 }
