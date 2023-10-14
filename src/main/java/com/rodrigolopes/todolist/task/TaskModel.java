53199f9ec66de0e4605a7226e0c1d917c8497eb0
jrrodrigo421 <rjr89000@gmail.com>
2023-10-13 22:17:17 -0300

    criando e salvando tarefas de um determinado usuario

--- src/main/java/com/rodrigolopes/todolist/task/TaskModel.java
+++ src/main/java/com/rodrigolopes/todolist/task/TaskModel.java
@@ -22,7 +22,7 @@ public class TaskModel {
   @Column(length = 50)
   private String title;
   private LocalDateTime startAt;
-  private LocalDateTime EndAt;
+  private LocalDateTime endAt;
   private String priority;
 
   @CreationTimestamp
